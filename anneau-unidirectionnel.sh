5849bcc78db73ed01765e351b0790c6f12882bc9
sesinian <antoine.sesini@etu.utc.fr>
2024-05-14 15:15:56 +0200

    Quelques modifications pour l'utilisateur du projet et du script de lancement en rése

--- anneau-unidirectionnel.sh
+++ anneau-unidirectionnel.sh
@@ -7,16 +7,16 @@ mkfifo /tmp/in_C2 /tmp/out_C2
 mkfifo /tmp/in_A3 /tmp/out_A3
 mkfifo /tmp/in_C3 /tmp/out_C3
 
-go run app-base -n A1 -m g< /tmp/in_A1 > /tmp/out_A1 &
-#go run app-base -n A1< /tmp/in_A1 > /tmp/out_A1 &
+#go run app-base -n A1 -m g< /tmp/in_A1 > /tmp/out_A1 &
+go run app-base -n A1< /tmp/in_A1 > /tmp/out_A1 &
 go run app-control -n C1 < /tmp/in_C1 > /tmp/out_C1 &
 
-go run app-base -n A2 -m g< /tmp/in_A2 > /tmp/out_A2 &
-#go run app-base -n A2< /tmp/in_A2 > /tmp/out_A2 &
+#go run app-base -n A2 -m g< /tmp/in_A2 > /tmp/out_A2 &
+go run app-base -n A2< /tmp/in_A2 > /tmp/out_A2 &
 go run app-control -n C2 < /tmp/in_C2 > /tmp/out_C2 &
 
-go run app-base -n A3 -m g< /tmp/in_A3 > /tmp/out_A3 &
-#go run app-base -n A3< /tmp/in_A3 > /tmp/out_A3 &
+#go run app-base -n A3 -m g< /tmp/in_A3 > /tmp/out_A3 &
+go run app-base -n A3< /tmp/in_A3 > /tmp/out_A3 &
 go run app-control -n C3 < /tmp/in_C3 > /tmp/out_C3 &
 
 cat /tmp/out_A1 > /tmp/in_C1 &
